273ad767e4f00e1f9eebec0d295bcf5ecb4fc1ab
Vasil-Ku <vaskuch84@gmail.com>
2020-09-24 18:59:20 +0300

    add scheme

---
+++ script.js
@@ -0,0 +1,15 @@
+const choise = document.querySelector("[data-spec-form]");
+const choiseDisplay = document.querySelector('[data-spec-display]');
+
+if (localStorage.spec != undefined) {
+  choiseDisplay.innerText = localStorage.spec;
+}
+
+choise.addEventListener('click', (event) => {
+  if (event.target.dataset.spec != undefined) {
+    localStorage.spec = event.target.innerText;
+    choiseDisplay.innerText = localStorage.spec;
+  }
+});
+
+alert(event.target.firstChild.value);

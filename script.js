30c924a8ce8a34ab1c5dad1884123504509fc79a
SonYk1993 <52713368+SonYk1993@users.noreply.github.com>
2020-10-01 11:47:17 +0300

    2 task

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
-function fun1() {
-    var chbox;
-    chbox=document.getElementById('one');
-        if (chbox.checked) {
-           document.getElementsById("li-ferst").getElementById="li-check";
+function change(){
+    // let h =document.getElementsByTagName("h1")[0];
+    // let count = 0;
+
+    let liBox = document.querySelectorAll(".box-li");
+    let chek = document.querySelectorAll(".addColor");
+    let inpColor = document.querySelectorAll(".inputCheck");
+    let knopka = document.querySelectorAll(".btn");
+    for(let i = 0; i<liBox.length; i++){
+             
+        if(chek[i].chacked){
+            liBox[i].style.backgroundColor = "orange"
+            inpColor[i].style.backgroundColor = "white"
+            knopka[i].innerHTML = "Done!";
+            knopka[i].style.border = "none";
+            knopka[i].style.backgroundColor  = "orange";
+            count++;
+            console.log('hello')
         }
-        else {
-            alert ('Не выбран');
+        else{
+            liBox[i].style.backgroundColor = "white";
+            inpColor[i].style.backgroundColor = "#FED230";
+            knopka[i].innerHTML = "Пройти";
+            knopka[i].style.border = "3px solid black";
+            knopka[i].style.backgroundColor = "white";
+    
         }
     }
+
+    
+    // switch (count) {
+    //     case 0:
+    //         h.innerHTML = 'Вы не прошли ни одного курса';
+    //         break;
+    //     case 1:
+    //         h.innerHTML = 'Вы прошли 1 курс';
+    //         break;
+    //     case 6:
+    //         h.innerHTML = 'Вы прошли все курсы';
+    //         break;
+    //     case 5:
+    //         h.innerHTML = 'Вы прошли 5 курсов';
+    //         break;
+    //     default:
+    //         h.innerHTML = `Вы прошли ${count} курса`;
+    // }
+   
+   
+
+}
+
+
+
+}

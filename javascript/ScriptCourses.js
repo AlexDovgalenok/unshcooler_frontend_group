77ad7a352200ac7153b57497ede178a966fc46eb
SergeyBozhok <67876729+SergeyBozhok@users.noreply.github.com>
2020-10-03 00:55:32 +0300

    ошибка с переходом на предыдущую страницу

---
+++ javascript/ScriptCourses.js
@@ -0,0 +1,43 @@
+
+
+var CHECKBOX = document.querySelectorAll('[type="checkbox"]');
+var CARDCOURSE = document.getElementsByClassName("card course");
+var DOC = document.forms.form;
+var CIRCLE = DOC.getElementsByClassName("circ");
+var BUTTON = DOC.getElementsByClassName("but");
+
+DOC.onchange = function () {
+    let count = 0;
+    h2 = document.getElementsByTagName("h2")[0];
+    for (let i = 0; i <6; i++) {
+        if (CHECKBOX[i].checked) {
+            CARDCOURSE[i].style.backgroundColor = '#FED230';
+            CIRCLE[i].style.backgroundColor = 'white';
+            BUTTON[i].innerHTML = 'Пройдено!';
+            BUTTON[i].style.borderColor = '#FED230';
+            count ++;
+        }
+        else {
+            CARDCOURSE[i].style.backgroundColor = 'white';
+            CIRCLE[i].style.backgroundColor = '#FED230';
+            BUTTON[i].innerHTML = 'Пройти';
+            BUTTON[i].style.borderColor = 'black';
+        }
+    }
+    switch (count) { 
+        case 0:
+            h2.innerHTML = 'Ты не прошел ни одного курса';
+            break;
+        case 1:
+            h2.innerHTML = 'Ты прошел 1 курс';
+            break;
+        case 6:
+            h2.innerHTML = 'Ты прошел все курсы';
+            break;
+        case 5:
+            h2.innerHTML = 'Ты прошел 5 курсов';
+            break;
+        default:
+            h2.innerHTML = `Ты прошел ${count} курса`;
+    }
+}
--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,26 @@
 (function(selector) {
-    // не дублируем код
     function save(data) {
         localStorage.setItem(selector, JSON.stringify(data));
+        
     }
-    // и не создаем тысячи функций в цикле
-    // а используем одну общую
+    
     function onChange(event) {
         var element = event.target,
             name = element.name,
             value = element.value;
-        data[name] = value;
+            h3 = document.getElementsByTagName("h3")[0];
+            data[name] = value;
+            h3.innerHTML = value;
         save(data);
+        
     }
     var elements = document.querySelectorAll(selector),
         data = localStorage.getItem(selector);
+        
     if(data) { // если в сторадже что-то есть
         // то можем и распарсить
         data = JSON.parse(data);
+        
     } else {
         // иначе парсить нельзя, будет ошибка
         // присвоим дефолтное значение и сохраним
@@ -25,14 +29,25 @@
     // обратимся к прототипу
     Array.prototype.forEach.call(elements, function(element) {
         var name = element.name,
-            value = element.value;
+            value = element.value,
+            h3 = document.getElementsByTagName("h3")[0];
+            
         if(data[name] === value) { // если текущий элемент отмечен в сторадже
             // то отметим и на странице
             element.checked = true;
+            h3.innerHTML = value;
+                        
         }
         // навесим созданый вне цикла хандлер на событие
         element.addEventListener("change", onChange); 
         
+        
+        
     });
+
+    
+        
+    
+
 })("input[type=radio]");
 
